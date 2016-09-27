1fb7d3fe7e22dfc7ca61e098d5a64d651553f00d
Ashish Goel <ashish@czonetechnologies.com>
2016-09-27 15:57:15 +0530

    Fixed selector

--- view/frontend/web/js/product/list/toolbar.js
+++ view/frontend/web/js/product/list/toolbar.js
@@ -23,7 +23,7 @@ define([
             directionControl: '[data-role="direction-switcher"]',
             orderControl: '[data-role="sorter"]',
             limitControl: '[data-role="limiter"]',
-            pagerControl: '[data-role="pager"], .pages-items .page',
+            pagerControl: '[data-role="pager"], .pages-items a',
             mode: 'product_list_mode',
             direction: 'product_list_dir',
             order: 'product_list_order',
